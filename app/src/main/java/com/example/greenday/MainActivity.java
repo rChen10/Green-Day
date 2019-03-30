ceddee9545f0f57f2214ac50f70136b0f5c1d6da
rchen10 <ricky.chen.2@stonybrook.edu>
2019-03-29 21:25:31 -0400

    worked on mainactivity

--- app/src/main/java/com/example/greenday/MainActivity.java
+++ app/src/main/java/com/example/greenday/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.greenday;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +18,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        collectionsFragment = new ManagerFragment();
-        collectionsFragment.setObject(mainManager);
+        // obtain saved info
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+
+        // find date
+        Calendar currentDate = Calendar.getInstance();
+
+        // prompt update if date is new
+        float defaultDriving = Float.parseFloat(getResources().getString(R.string.minutes_driving_default));
+        float minutesDriving = sharedPref.getFloat(getString(R.string.minutes_driving), defaultDriving);
+
+        // read from info otherwise
+
+
+        Fragment = new ManagerFragment();
+        Fragment.setObject(mainManager);
         android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
         android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.add(R.id.fragment_container, collectionsFragment);
