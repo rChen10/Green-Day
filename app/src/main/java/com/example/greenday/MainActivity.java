8b7e715505e73b2cd8715989a3905664af365ab9
rchen10 <ricky.chen.2@stonybrook.edu>
2019-03-29 20:35:33 -0400

    ported old fragment code

--- app/src/main/java/com/example/greenday/MainActivity.java
+++ app/src/main/java/com/example/greenday/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.greenday;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        collectionsFragment = new ManagerFragment();
+        collectionsFragment.setObject(mainManager);
+        android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+        android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container, collectionsFragment);
+        fragmentTransaction.commit();
     }
 }
